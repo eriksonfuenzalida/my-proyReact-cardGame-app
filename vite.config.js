7439f615f3707f80a860507b15efb141d626e87d
eriksonfuenzalida <erikson.fuenzalida@gmail.com>
2023-03-06 23:35:19 -0300

    Correxion Final

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,10 +20,8 @@ export default defineConfig({
     chunks: true,
     moduleBundling: true,
     modulePreload: true,
-    prerenderPaths: ["/"],
-    outDir: "build",
     mode: "production",
     debug: true,
     devCode: true,
   },
-})
+});
